8fe5adb6d91fc0c526ef10655b44af556176a803
david-garza <locodave@gmail.com>
2022-05-06 13:35:49 -0500

    Successful read.csv

---
+++ MechaCarChallenge.R
@@ -0,0 +1,5 @@
+# load needed libraries, using tidyverse as it also provides ggplot
+library(tidyverse)
+
+# Load the data, all csv files are ignored by git
+mechadata <- read.csv("data/MechaCar_mpg.csv")

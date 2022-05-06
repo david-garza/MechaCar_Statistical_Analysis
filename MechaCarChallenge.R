fb8fc44d6d814007376b247a44abfda069aacaf2
david-garza <locodave@gmail.com>
2022-05-06 14:28:48 -0500

    modeling and summary

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -10,3 +10,9 @@ cor(mechadata)
 # Visually inspect correlations
 ggplot(mechadata,aes(y=mpg,x=vehicle_length))+geom_point()
 ggplot(mechadata,aes(y=mpg,x=ground_clearance))+geom_point()
+
+# Linear regression modeling
+model<-lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,mechadata)
+
+# Model summary
+summary(model)
